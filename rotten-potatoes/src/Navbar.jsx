e0ce0836d81ec6c025468c1a029380de4fba21cc
eeds90 <eddie.diaz90@icloud.com>
2020-10-16 12:50:25 -0500

    CSS

--- rotten-potatoes/src/Navbar.jsx
+++ rotten-potatoes/src/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <div className="Navbar">
     <header>
-      <h1>Movies in Theaters</h1>
+      <h1>New Releases</h1>
       <h2>*Hover over for movie overview.</h2>
       <nav>
       </nav>
